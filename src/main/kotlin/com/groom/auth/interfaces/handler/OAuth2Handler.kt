426fe5d88d346e38379529913a351ec67e07f02a
jsween5723 <jsween5723@gmail.com>
2025-06-29 17:21:17 +0900

    refactor(oauth2): OAuth2 핸들러 병합

--- src/main/kotlin/com/groom/auth/interfaces/handler/OAuth2Handler.kt
+++ src/main/kotlin/com/groom/auth/interfaces/handler/OAuth2Handler.kt
@@ -1,17 +1,18 @@
 package com.groom.auth.interfaces.handler
 
-import com.groom.auth.application.AuthCriteria
-import com.groom.auth.application.AuthFacade
 import com.groom.auth.component.JwtGenerator
+import com.groom.auth.domain.authentication.AuthCommand
+import com.groom.auth.domain.authentication.AuthenticationService
 import com.groom.auth.domain.authentication.OAuth2ProviderName
 import com.groom.auth.interfaces.api.AuthResponse
-import com.groom.auth.interfaces.api.CustomOAuth2User
 import com.groom.common.ResponseSender
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
 import org.springframework.security.core.Authentication
-import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserService
 import org.springframework.security.oauth2.core.user.OAuth2User
@@ -22,38 +23,28 @@ import java.time.Instant
 internal class OAuth2Handler(
     private val jwtGenerator: JwtGenerator,
     private val responseSender: ResponseSender,
-    private val authFacade: AuthFacade,
-) : OAuth2UserService<OAuth2UserRequest, OAuth2User> {
-    private val oAuth2UserService: DefaultOAuth2UserService = DefaultOAuth2UserService()
-
+    private val oAuth2AuthorizedClientService: OAuth2AuthorizedClientService,
+    private val authenticationService: AuthenticationService
+) {
     private val logger = LoggerFactory.getLogger(OAuth2Handler::class.java)
     val oAuth2successLoginHandler =
         { _: HttpServletRequest, response: HttpServletResponse, authentication: Authentication ->
             logger.info(authentication.toString())
-            val oauth2User = authentication.principal as CustomOAuth2User
-            val accessToken =
-                jwtGenerator.generate(oauth2User.authentication.claims, Instant.now())
+            val oauth2User = authentication as OAuth2AuthenticationToken
+            val registrationId = oauth2User.authorizedClientRegistrationId
+            val client = oAuth2AuthorizedClientService.loadAuthorizedClient<OAuth2AuthorizedClient>(
+                registrationId, oauth2User.name
+            )
+            val providerName = OAuth2ProviderName.fromName(
+                registrationId
+            )
+            val command = AuthCommand.OAuth2LoginWithAccessToken(
+                providerName, client.accessToken.tokenValue, client.accessToken.scopes
+            )
+            val resourceAuthentication = authenticationService.loginWithOAuth2Token(
+                command
+            )
+            val accessToken = jwtGenerator.generate(resourceAuthentication.claims, Instant.now())
             responseSender.send(response, AuthResponse.Login(accessToken))
         }
-
-    override fun loadUser(userRequest: OAuth2UserRequest): OAuth2User {
-        val oAuth2User = oAuth2UserService.loadUser(userRequest)
-        val attributes = oAuth2User.attributes
-        val providerName = OAuth2ProviderName.fromName(userRequest.clientRegistration.clientName)
-        val nameAttributeName =
-            userRequest.clientRegistration
-                .providerDetails
-                .userInfoEndpoint
-                .userNameAttributeName
-        val authentication =
-            authFacade.loginOrSignUpForOAuth2(
-                AuthCriteria.OAuth2Login(
-                    accessToken = userRequest.accessToken.tokenValue,
-                    providerName = providerName,
-                    providerUserId = oAuth2User.name,
-                    attributes = attributes,
-                ),
-            )
-        return CustomOAuth2User(authentication, attributes, nameAttributeName)
-    }
 }
